16605caf07417396358fc9a925493eed48b05440
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-04-26 20:40:33 +0200

    StringBuffer

---
+++ src/main/java/StringMutable.java
@@ -0,0 +1,9 @@
+public class StringMutable {
+    public static void main(String[] args) {
+
+        StringBuffer st = new StringBuffer("lukasz");
+        st.append(" kowalski");
+
+        System.out.println(st);
+    }
+}

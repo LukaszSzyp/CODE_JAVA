78ce50735dd3a0c40fc5a96032699f9d0b50a260
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-05-18 20:43:06 +0200

    instruction break with marking place to get out

--- src/main/java/StringMutable.java
+++ src/main/java/StringMutable.java
@@ -2,15 +2,17 @@ public class StringMutable {
     public static void main(String[] args) {
 
         String st = new String("lukasz");
-        st +=" kowalski";
+        st += " kowalski";
 
         System.out.println(st);
 
-        for (int i = 0; i < 10; i++) {
-            if (i==5){
-                break;
+        mark: for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 10; i++) {
+                if (i == 5) {
+                    break mark;
+                }
+                System.out.println(i);
             }
-            System.out.println(i);
         }
         System.out.println("more");
     }

2f03c3479d8fe4525bb848548047b28dfe115e4b
LukaszSzyp <54202919+LukaszSzyp@users.noreply.github.com>
2020-05-24 14:41:06 +0200

    chars method

--- src/main/java/StringMutable.java
+++ src/main/java/StringMutable.java
@@ -1,20 +1,9 @@
+import java.util.Arrays;
+
 public class StringMutable {
     public static void main(String[] args) {
-
-       String word = "something";
-
-       char[] chars = word.toCharArray();
-
-       System.out.println(chars.length);
-
-       char sign = word.charAt(2);
-
-       System.out.println(sign);
-
-       char[] chars1 = new char[3];
-       word.getChars(0,3,chars1,0);
-       System.out.println(chars1);
-
+        int i = 0b010_00_00;
+        System.out.println(i);
 
     }
 }
